8e58846f922dd69add74765b9e2d68d740cfbb63
Chris West (Faux) <git@goeswhere.com>
2014-02-18 19:42:22 +0000

    parse config and show the pressed key

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import "C"
 
 import (
     "fmt"
+    "bufio"
+    "log"
+    "os"
+    "strconv"
+    "encoding/json"
 )
 
 var output = make(chan string, 100)
@@ -19,11 +24,42 @@ func chordReleased(bytes *C.char) {
 
 func main() {
     go C.setup();
+    fi, err := os.Open("config.json")
+    if nil != err {
+        fi2, err2 := os.Open("config.json.template")
+        if nil != err2 {
+            log.Fatal("no config.json: ", err, ", nor config.json.template: ", err2)
+        }
+        fi = fi2
+    }
+
+    r := bufio.NewReader(fi)
+    dec := json.NewDecoder(r)
+
+    type Config struct {
+        Keys map[string]string
+    }
+
+    var c Config
+    err = dec.Decode(&c)
+    if nil != err {
+        log.Fatal("couldn't unmarshal: ", err)
+    }
+
+    var keyMap [255]string
+    for key, value := range c.Keys {
+        val, err := strconv.Atoi(key)
+        if nil != err {
+            log.Fatal("vkeys must be integers: ", err)
+        }
+        keyMap[val] = value
+    }
+
     for {
         req := <-output
         // this cannot use range as it is not utf-8
         for i := 0; i < len(req); i++ {
-            fmt.Printf("%x ", req[i])
+            fmt.Printf("%d: %s ", req[i], keyMap[req[i]])
         }
         fmt.Println()
     }
