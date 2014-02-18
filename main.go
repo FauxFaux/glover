7036d93fce8085dcb2651880522f9f3ab1208ec0
Chris West (Faux) <git@goeswhere.com>
2014-02-18 21:05:21 +0000

    working key mapping (qwerty)

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
     "bufio"
     "log"
     "os"
-    "strconv"
     "encoding/json"
 )
 
@@ -22,8 +21,12 @@ func chordReleased(bytes *C.char) {
     output <- C.GoString(bytes)
 }
 
+type StenoKey uint8
+type QwertyKey uint8
+type VKey uint8
+
 const (
-    _ = iota
+    _ StenoKey = iota
     LS
     LT
     LK
@@ -45,32 +48,79 @@ const (
     RS
     RD
     RZ
+    NUMBER
+    STAR
 )
 
-var wtbEnum = map[string]uint8 {
-    "LS": LS,
-    "LT": LT,
-    "LK": LK,
-    "LP": LP,
-    "LW": LW,
-    "LH": LH,
-    "LR": LR,
-    "LA": LA,
-    "LO": LO,
-    "RE": RE,
-    "RU": RU,
-    "RF": RF,
-    "RR": RR,
-    "RP": RP,
-    "RB": RB,
-    "RL": RL,
-    "RG": RG,
-    "RT": RT,
-    "RS": RS,
-    "RD": RD,
-    "RZ": RZ,
+var toEnum = map[string]StenoKey {
+    "S-": LS,
+    "T-": LT,
+    "K-": LK,
+    "P-": LP,
+    "W-": LW,
+    "H-": LH,
+    "R-": LR,
+    "A-": LA,
+    "O-": LO,
+    "-E": RE,
+    "-U": RU,
+    "-F": RF,
+    "-R": RR,
+    "-P": RP,
+    "-B": RB,
+    "-L": RL,
+    "-G": RG,
+    "-T": RT,
+    "-S": RS,
+    "-D": RD,
+    "-Z": RZ,
+    "#":  NUMBER,
+    "*":  STAR,
 }
 
+func reverse(in map[string]StenoKey) map[StenoKey]string {
+    ret := map[StenoKey]string{}
+    for key, value := range in {
+        ret[value] = key
+    }
+    return ret
+}
+
+var fromEnum = reverse(toEnum)
+
+func vkeyToChar(in VKey) QwertyKey {
+    if in >= 65 && in < 91 {
+        return QwertyKey(in - 65 + 'a')
+    }
+
+    if in >= 48 && in < 58 {
+        return QwertyKey(in - 48 + '0')
+    }
+
+    return 0
+}
+
+func keyNameToVKey(in string) VKey {
+    var from QwertyKey = QwertyKey(in[0])
+    if from >= 'a' && from <= 'z' {
+        return VKey(from - 'a' + 65)
+    }
+
+    if from >= '0' && from <= '9' {
+        return VKey(from - '0' + 0x30)
+    }
+
+    switch from {
+        case ';': return 0xba
+        case '[': return 0xdb
+        case ']': return 0xdd
+        case '-': return 0xbd
+        case '\'': return 0xde
+    }
+
+    log.Fatalf("don't understand '%c' as a VKey", from)
+    return 0
+}
 
 func main() {
     go C.setup();
@@ -96,13 +146,10 @@ func main() {
         log.Fatal("couldn't unmarshal: ", err)
     }
 
-    var keyMap [255]uint8
+    var keyMap = map[VKey]StenoKey{}
     for key, value := range c.Keys {
-        val, err := strconv.Atoi(key)
-        if nil != err {
-            log.Fatal("vkeys must be integers: ", err)
-        }
-        var x = wtbEnum[value]
+        var val VKey = keyNameToVKey(key)
+        var x StenoKey = toEnum[value]
         if 0 == x {
             log.Fatalf("I can't parse '%s' as a steno key", value)
         }
@@ -114,7 +161,11 @@ func main() {
         req := <-output
         // this cannot use range as it is not utf-8
         for i := 0; i < len(req); i++ {
-            fmt.Printf("%d: %s ", req[i], keyMap[req[i]])
+            var sk StenoKey = keyMap[VKey(req[i])]
+            if 0 == sk {
+                continue
+            }
+            fmt.Printf("%c: %s;   ", req[i], fromEnum[sk])
         }
         fmt.Println()
     }
