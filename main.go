a518907516271b198ebf5ce4ad39d4ead0eee756
Chris West (Faux) <git@goeswhere.com>
2014-02-24 09:37:22 +0000

    lift some config parsing stuff into readdata

--- main.go
+++ main.go
@@ -60,32 +60,6 @@ const (
 	STAR
 )
 
-var toEnum = map[string]StenoKey{
-	"S-": LS,
-	"T-": LT,
-	"K-": LK,
-	"P-": LP,
-	"W-": LW,
-	"H-": LH,
-	"R-": LR,
-	"A-": LA,
-	"O-": LO,
-	"-E": RE,
-	"-U": RU,
-	"-F": RF,
-	"-R": RR,
-	"-P": RP,
-	"-B": RB,
-	"-L": RL,
-	"-G": RG,
-	"-T": RT,
-	"-S": RS,
-	"-D": RD,
-	"-Z": RZ,
-	"#":  NUMBER,
-	"*":  STAR,
-}
-
 var toKeyName = map[StenoKey]string{
 	LS:     "S",
 	LT:     "T",
@@ -125,16 +99,6 @@ var numResolve = map[byte]StenoKey{
 	'0': RD,
 	'#': NUMBER,
 }
-
-func reverse(in map[string]StenoKey) map[StenoKey]string {
-	ret := map[StenoKey]string{}
-	for key, value := range in {
-		ret[value] = key
-	}
-	return ret
-}
-
-var fromEnum = reverse(toEnum)
 
 func vkeyToChar(in VKey) QwertyKey {
 	if in >= 65 && in < 91 {
@@ -187,59 +151,6 @@ func openJson(name string) (dec *json.Decoder, fi *os.File) {
 
 	r := bufio.NewReader(fi)
 	return json.NewDecoder(r), fi
-}
-
-func h(s StenoKey) Chord {
-	return 1 << s
-}
-
-func splitChar(c byte, passedMid bool) (ret Chord, newMid bool, err error) {
-	switch c {
-	case 'A':
-		return h(LA), true, nil
-	case 'O':
-		return h(LO), true, nil
-	case 'E':
-		return h(RE), true, nil
-	case 'U':
-		return h(RU), true, nil
-	case '*':
-		return h(STAR), true, nil
-	case '-':
-		return 0, true, nil
-	}
-
-	foundNum := numResolve[c]
-	if 0 != foundNum {
-		return h(NUMBER) | h(foundNum), true, nil
-	}
-
-	var encoding string
-	if passedMid {
-		encoding = fmt.Sprintf("-%c", c)
-	} else {
-		encoding = fmt.Sprintf("%c-", c)
-	}
-
-	found := toEnum[encoding]
-	if 0 != found {
-		return h(found), passedMid, nil
-	}
-
-	return 0, passedMid, fmt.Errorf("not happy with '%c' %t", c, passedMid)
-}
-
-func parseChord(s string) (ret Chord, err error) {
-	passedMid := false
-	for i := 0; i < len(s); i++ {
-		ch, newMid, err := splitChar(s[i], passedMid)
-		passedMid = newMid
-		if nil != err {
-			return 0, fmt.Errorf("Can't read '%c' in '%s': %s", s[i], s, err)
-		}
-		ret |= ch
-	}
-	return ret, nil
 }
 
 func load(dict map[string]string) (chords Sequence) {
@@ -333,16 +244,7 @@ func main() {
 
 	chords := load(dict)
 
-	var keyMap = map[VKey]StenoKey{}
-	for key, value := range c.Keys {
-		var val VKey = keyNameToVKey(key)
-		var x StenoKey = toEnum[value]
-		if 0 == x {
-			log.Fatalf("I can't parse '%s' as a steno key", value)
-		}
-
-		keyMap[val] = x
-	}
+	keyMap := readKeys(c.Keys)
 
 	for k, v := range chords.Predecessors {
 		if v.Value == "have" {
@@ -362,7 +264,6 @@ func main() {
 				continue
 			}
 			c |= h(sk)
-			//			fmt.Printf("%c: %s;   ", vk, fromEnum[sk])
 		}
 		if 0 != c {
 			prev.Value = c
