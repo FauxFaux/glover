f3bca0674b4d6567b14a2a50f0dbb184a700ace0
Chris West (Faux) <git@goeswhere.com>
2014-02-25 19:00:57 +0000

    update tests for kill

--- main_test.go
+++ main_test.go
@@ -14,6 +14,15 @@ func assertEquals(t *testing.T, expected, actual string) {
 	t.Fatalf("'%s' must equal '%s'", actual, expected)
 }
 
+func assertIEquals(t *testing.T, expected, actual int) {
+	if expected == actual {
+		return
+	}
+
+	t.Fatalf("'%d' must equal '%d'", actual, expected)
+}
+
+
 func dump(s Sequence, depth int) {
 	fmt.Println(s.Value)
 	for k, v := range s.Predecessors {
@@ -47,21 +56,27 @@ func TestLoad(t *testing.T) {
 	assertEquals(t, "rat", out.Predecessors[RAT].Value)
 }
 
+func assertKill(t *testing.T, expected int, s Sequence, r *ring.Ring) string {
+	ret, kill := lookup(s, r, -1)
+	assertIEquals(t, expected, kill)
+	return ret
+}
+
 func TestLookup(t *testing.T) {
 	out := load(one)
 	r := ring.New(5)
 	r.Value = SR
 	r = r.Next()
-	assertEquals(t, "have", lookup(out, r))
+	assertEquals(t, "have", assertKill(t, 0, out, r))
 	r.Value = RAT
 	r = r.Next()
-	assertEquals(t, "rat", lookup(out, r))
+	assertEquals(t, "rat", assertKill(t, 0, out, r))
 	r.Value = SEP
 	r = r.Next()
-	assertEquals(t, "SEP", lookup(out, r))
+	assertEquals(t, "SEP", assertKill(t, 0, out, r))
 	r.Value = RAT
 	r = r.Next()
-	assertEquals(t, "separate", lookup(out, r))
+	assertEquals(t, "separate", assertKill(t, 1, out, r))
 }
 
 func TestJodieFoster(t *testing.T) {
@@ -71,7 +86,7 @@ func TestJodieFoster(t *testing.T) {
 		r.Value = S
 		r = r.Next()
 	}
-	assertEquals(t, "Jodie Foster", lookup(out, r))
+	assertEquals(t, "Jodie Foster", assertKill(t, 6, out, r))
 }
 
 /* vim: set noexpandtab: */
